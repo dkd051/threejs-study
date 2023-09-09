d97787cf29c4c4e5314d5984f8a1d5561372f92f
Koo <koo@brightn.io>
2023-09-10 01:31:19 +0900

    브라우저 창 크기 조정 대응 pixel ratio 설정

--- basic/src/ex01.js
+++ basic/src/ex01.js
@@ -2,11 +2,11 @@ import * as THREE from 'three';
 
 export default function example() {
     // 동적으로 캔버스 조립하기
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
+    // const renderer = new THREE.WebGLRenderer();
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild(renderer.domElement);
 
-// html에서 캔버스 가져와서 사용하기
+    // html에서 캔버스 가져와서 사용하기
     const canvas = document.querySelector('#three-canvas');
     const renderer = new THREE.WebGLRenderer({
         canvas,
@@ -14,22 +14,22 @@ export default function example() {
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Scene
+    // Scene
     const scene = new THREE.Scene();
 
-// Camera
-// Perspective Camera(원근 카메라)
-// const camera = new THREE.PerspectiveCamera(
-//     75,
-//     window.innerWidth / window.innerHeight,
-//     0.1,
-//     1000
-// );
-// camera.position.x = 1;
-// camera.position.y = 2;
-// camera.position.z = 5;
+    // Camera
+    // Perspective Camera(원근 카메라)
+    // const camera = new THREE.PerspectiveCamera(
+    //     75,
+    //     window.innerWidth / window.innerHeight,
+    //     0.1,
+    //     1000
+    // );
+    // camera.position.x = 1;
+    // camera.position.y = 2;
+    // camera.position.z = 5;
 
-// Orthographic Camera(직교 카메라)
+    // Orthographic Camera(직교 카메라)
     const camera = new THREE.OrthographicCamera(
         -(window.innerWidth / window.innerHeight), // left
         (window.innerWidth / window.innerHeight), // right,
@@ -46,7 +46,7 @@ export default function example() {
     camera.updateProjectionMatrix();
     scene.add(camera);
 
-// Mesh
+    // Mesh
     const geometry = new THREE.BoxGeometry(1, 1, 1,);
     const material = new THREE.MeshBasicMaterial({
         color: 'red'
@@ -54,6 +54,6 @@ export default function example() {
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
-// 그리기
+    // 그리기
     renderer.render(scene, camera);
 }
---
+++ basic/src/ex02.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three';
+
+export default function example() {
+    // 동적으로 캔버스 조립하기
+    // const renderer = new THREE.WebGLRenderer();
+    // renderer.setSize(window.innerWidth, window.innerHeight);
+    // document.body.appendChild(renderer.domElement);
+
+    // html에서 캔버스 가져와서 사용하기
+    const canvas = document.querySelector('#three-canvas');
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // console.log(window.devicePixelRatio);
+    renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    // Camera
+    // Perspective Camera(원근 카메라)
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    camera.position.x = 1;
+    camera.position.y = 2;
+    camera.position.z = 5;
+
+    // Orthographic Camera(직교 카메라)
+    // const camera = new THREE.OrthographicCamera(
+    //     -(window.innerWidth / window.innerHeight), // left
+    //     (window.innerWidth / window.innerHeight), // right,
+    //     1, // top
+    //     -1, // bottom
+    //     0.1,
+    //     100
+    // );
+    // camera.position.x = 1;
+    // camera.position.y = 2;
+    // camera.position.z = 5;
+    // camera.lookAt(0, 0, 0);
+    // camera.zoom = 0.5;
+    // camera.updateProjectionMatrix();
+    // scene.add(camera);
+
+    // Mesh
+    const geometry = new THREE.BoxGeometry(1, 1, 1,);
+    const material = new THREE.MeshBasicMaterial({
+        color: 'red'
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // 그리기
+    renderer.render(scene, camera);
+
+    function setSize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.render(scene, camera);
+    }
+    // 이벤트
+    window.addEventListener('resize', setSize);
+}
